8f65323e57045d83abd88202fa2cbb1c733fa715
jahankhanna <jahankhanna@gmail.com>
2022-03-25 19:59:31 -0700

    halfway state

--- src/DAOCreationForm.js
+++ src/DAOCreationForm.js
@@ -1,5 +1,5 @@
 //reactJS reqs
-import {React, Component, useState} from 'react';
+import * as React from 'react';
 
 //material UI reqs
 import Grid from "@mui/material/Grid";
@@ -19,6 +19,7 @@ import ImageListItemBar from '@mui/material/ImageListItemBar';
 import ListSubheader from '@mui/material/ListSubheader';
 import IconButton from '@mui/material/IconButton';
 import Avatar from '@mui/material/Avatar';
+import AvatarGroup from '@mui/material/AvatarGroup';
 import Stack from '@mui/material/Stack';
 import { deepOrange } from '@mui/material/colors';
 
@@ -26,6 +27,7 @@ import { deepOrange } from '@mui/material/colors';
 import Uploady, { useItemProgressListener } from "@rpldy/uploady";
 import UploadButton from "@rpldy/upload-button";
 import UploadPreview from "@rpldy/upload-preview";
+
 
 //must be rendered inside <Uploady>
 const LogProgress = () => {
@@ -36,8 +38,31 @@ const LogProgress = () => {
     return null;
 }
 
+/*
+//form handler logic
+const [formValues, setFormValues] = useState(defaultValues);
 
-class DAOCreationForm extends Component{
+//slider UX handler
+const handleSliderChange = (name) => (e, value) => {
+    setFormValues({
+    ...formValues,
+    [name]: value,
+    });
+};
+*/
+
+class DAOCreationForm extends React.Component{
+    
+    
+    handleChange(event: React.ChangeEvent<HTMLInputElement>){
+        //setValue(event.target.value);
+    };
+
+    constructor(props){
+        super(props);
+        //this.value = React.useState('Controlled')[0];
+        //this.setValue = React.useState('Controlled')[1];
+    }
 
     render(){
         return(
@@ -45,64 +70,91 @@ class DAOCreationForm extends Component{
                 <form>
                 <Grid container alignItems="center" justify="center" direction="column">
                     <Grid item>
-                    <h1 style={{background:'white',color:'red'}}>John Doe</h1>
-                    <h3 style={{color:'white'}}>Teacher</h3>
+                    <h1 style={{background:'white',color:'red'}}>Create your DAO</h1>
                     </Grid>
                     <Grid item>
-                    <Stack direction="row" spacing={2}>
-                        <Avatar
-                            sx={{ bgcolor: deepOrange[500] }}
-                            alt="Remy Sharp"
-                            src="/broken-image.jpg"
-                        >
-                            B
-                        </Avatar>
-                        <Avatar
-                            sx={{ bgcolor: deepOrange[500] }}
-                            alt="Remy Sharp"
-                            src="/broken-image.jpg"
-                        />
-                        <Avatar src="/broken-image.jpg" />
-                    </Stack>
                     <TextField
-                        id="name-input"
-                        name="name"
-                        label="Name"
+                        id="DAOName-input"
+                        name="DAOName"
+                        label="Name of DAO"
                         type="text"
+                        placeholder="Chad's DAO"
                         value=""
-                        onChange=""
+                        onChange="handleChange"
                     />
                     </Grid>
                     <Grid item>
-                    <FormControl>
-                        <FormLabel>Gender</FormLabel>
+                        <TextField
+                        id="DAODescription"
+                        label="Description of DAO"
+                        placeholder="The world's largest community of Chads"
+                        multiline
+                        />
+                    </Grid>
+                    <Grid item>
+                        <FormControl>
+                        <FormLabel>Create new wallet for this DAO?</FormLabel>
                         <RadioGroup
-                        name="gender"
+                        name="DAOWalletType"
                         value=""
                         onChange=""
                         row
                         >
                         <FormControlLabel
-                            key="male"
-                            value="male"
+                            key="createNewWallet"
+                            value="createNewWallet"
                             control={<Radio size="small" />}
-                            label="Male"
+                            label="Create a Wallet for me"
                         />
                         <FormControlLabel
-                            key="female"
-                            value="female"
+                            key="bringWallet"
+                            value="bringWallet"
                             control={<Radio size="small" />}
-                            label="Female"
-                        />
-                        <FormControlLabel
-                            key="other"
-                            value="other"
-                            control={<Radio size="small" />}
-                            label="Other"
+                            label="I want to connect my wallet"
                         />
                         </RadioGroup>
-                    </FormControl>
+                        </FormControl>
                     </Grid>
+                    <Grid item>
+                        <TextField
+                        id="WalletAddr"
+                        label="Wallet Adress"
+                        placeholder="0x000"
+                        disabled={true}
+                        />
+                    </Grid>
+                    <Grid item>
+                        <FormControl>
+                        <FormLabel>Founding Member NFT Image</FormLabel>
+                        <Uploady
+                        destination={{ url: "https://my-server/upload" }}>
+                            <UploadPreview
+            fallbackUrl="https://icon-library.net/images/image-placeholder-icon/image-placeholder-icon-6.jpg"/>
+                            <LogProgress/>   
+                            <UploadButton/>
+                        </Uploady>
+                        </FormControl>
+                    </Grid>
+
+                    <Grid item>
+                    <h2 style={{background:'white',color:'red'}}>DAO Founders</h2>
+                    </Grid>
+
+                    <Grid item>
+                        <AvatarGroup max={4}>
+                            {this.itemData.map((item) => (
+                            <Avatar
+                                sx={{ width: 56, height: 56 }}
+                                alt={item.author}
+                                src={item.img}
+                            >
+                                
+                            </Avatar>
+                            ))}
+                        </AvatarGroup>
+                    </Grid>
+
+
                     <Grid item>
                     <FormControl>
                         <Select
